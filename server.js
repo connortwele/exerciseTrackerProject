08fbd7e5e4a9de4f45b5528ad2100fc9d3a6920b
Connor Twele <connortwele@gmail.com>
2020-09-09 09:22:34 -0400

    changed middleware

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('client/build'));
 
 //end middleware ^
 
@@ -27,7 +28,7 @@ const usersRouter = require('./routes/users')
 
 app.use('/exercises',exercisesRouter)
 app.use('/users', usersRouter)
-app.use(express.static('client/build'));
+
 
 //end routes
 
